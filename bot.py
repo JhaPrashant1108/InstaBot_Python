9ef011f7a3c9ee18c08cc94d703d3d536f77976e
JhaPrashant1108 <prashant.jha0008@gmail.com>
2020-02-09 19:43:05 +0530

    InstaBot_Python

---
+++ bot.py
@@ -0,0 +1,160 @@
+from selenium import webdriver
+import urllib.request
+import os
+import time
+from collections import OrderedDict
+
+class InstagramBot:
+   def __init__(self,Order):
+      print(Order)
+      self.username = Order[0]
+      self.password = Order[1]
+      self.user = Order[2]
+      self.Command = Order[3]
+      self.basic_url= 'https://www.instagram.com'
+      self.driver = webdriver.Chrome('chromedriver.exe')
+      self.login()
+
+   def login(self):
+      self.driver.get('{}/accounts/login/'.format(self.basic_url))
+      time.sleep(10)
+      self.driver.find_element_by_name('username').send_keys(self.username)
+      self.driver.find_element_by_name('password').send_keys(self.password)
+      self.driver.find_element_by_xpath('//*[@id="react-root"]/section/main/div/article/div/div[1]/div/form/div[4]').click()
+      time.sleep(10)
+      self.navigate_profile()
+
+   def navigate_profile(self):
+      self.driver.get('{}/{}/'.format(self.basic_url,self.user))
+      time.sleep(5)
+      #self.follow_profile()
+      #self.photo_liker()
+      self.worker()
+   
+   def worker(self):
+      if(self.Command == '0000' or self.Command == '1100'):
+         print("what do you really wanna do???")
+      elif(self.Command == '0011' or self.Command == '1111'):
+         self.initiate_download_image()
+         self.photo_liker()
+      elif(self.Command == '0010' or self.Command == '1110'):
+         self.photo_liker()
+      elif(self.Command == '0001' or self.Command == '1101'):
+         self.initiate_download_image()
+      elif(self.Command == '1011'):
+         self.follow_profile()
+         self.initiate_download_image()
+         self.photo_liker()
+      elif(self.Command == '0111'):
+         self.initiate_download_image()
+         self.photo_liker()
+         self.unfollow_profile()
+      elif(self.Command == '1010'):
+         self.follow_profile()
+         self.photo_liker()
+      elif(self.Command == '1001'):
+         self.follow_profile()
+         self.initiate_download_image()
+      elif(self.Command == '0110'):
+         self.photo_liker()
+         self.unfollow_profile()
+      elif(self.Command == '0101'):
+         self.initiate_download_image()
+         self.unfollow_profile()
+      elif(self.Command == '0100'):
+         self.unfollow_profile()
+      else:
+         self.follow_profile()
+      self.driver.close() 
+      
+         
+
+        
+    
+   def follow_profile(self):
+      follow_button = self.driver.find_element_by_xpath('//*[contains(concat( " ", @class, " " ), concat( " ", "_6VtSN", " " ))]')
+      follow_button.click()
+      time.sleep(5)
+    
+   def initiate_download_image(self):
+      img_srcs = []
+      finished = False
+      while not finished:
+
+         finished = self.bottom_scroll()
+
+         img_srcs.extend([img.get_attribute('src') for img in self.driver.find_elements_by_class_name('FFVAD')]) 
+
+      img_srcs = list(OrderedDict.fromkeys(img_srcs))
+
+      for index, src in enumerate(img_srcs):
+         self.download_image(src, index, self.user)
+        
+      self.top_scroll()
+        
+   def download_image(self, src, image_filename, folder):
+      folder_path = './{}'.format(folder)
+      if not os.path.exists(folder_path):
+         os.mkdir(folder_path)
+
+      urllib.request.urlretrieve(src, '{}/{}'.format(folder, 'image_{}.jpg'.format(image_filename)))
+
+
+   def bottom_scroll(self):
+        
+      SCROLL_PAUSE_TIME = 5
+
+      self.last_height = self.driver.execute_script("return document.body.scrollHeight")
+
+      self.driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+      time.sleep(SCROLL_PAUSE_TIME)
+
+      self.new_height = self.driver.execute_script("return document.body.scrollHeight")
+
+
+      if self.new_height == self.last_height:
+         return True
+
+      self.last_height = self.new_height
+      return False
+
+   def top_scroll(self):
+      SCROLL_PAUSE_TIME = 1
+      self.last_height = self.driver.execute_script("return document.body.scrollHeight")
+      self.driver.execute_script("window.scrollTo(document.body.scrollHeight,0);")
+      time.sleep(SCROLL_PAUSE_TIME)
+      self.new_height = self.driver.execute_script("return document.body.scrollHeight")
+      if self.new_height == self.last_height:
+         return True
+      self.last_height = self.new_height
+      return False
+    
+    
+   def unfollow_profile(self):
+      finished = False
+      while not finished:
+         finished = self.top_scroll()
+      unfollow_button = self.driver.find_elements_by_xpath("//*[text()='{}']".format('Following'))
+      unfollow_button[0].click()
+      time.sleep(2)
+      unfollow_confirm = self.driver.find_element_by_xpath('/html/body/div[3]/div/div/div[3]/button[1]')
+      unfollow_confirm.click()
+      time.sleep(10)
+      
+    
+   
+   def photo_liker(self):
+      self.driver.find_elements_by_class_name('_9AhH0')[0].click()
+      time.sleep(5)
+      self.driver.find_element_by_xpath("//*[@aria-label='{}']".format('Like')).click()
+      time.sleep(5)
+      self.driver.find_elements_by_class_name('ckWGn')[0].click()
+      time.sleep(5)
+
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,124 @@
+from bot import *
+import tkinter as tk
+from tkinter import font
+
+class App(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self._frame = None
+        self.switch_frame(StartPage)
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self._frame is not None:
+            self._frame.destroy()
+        self._frame = new_frame
+        self._frame.pack()
+
+class StartPage(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        appHighlightFont = font.Font(family='Helvetica', size=20)
+        lable1 = tk.Label(self , text="Hi There!",font=appHighlightFont)
+        lable1.pack()
+        
+        lable = tk.Label(self , text="",font=appHighlightFont)
+        lable.pack()
+        appHighlightFont = font.Font(family='Helvetica', size=13)
+        label_2 = tk.Message(self, text="Lorem ipsum dolor sit amet," 
+        " consectetur adipiscing elit, sed do eiusmod tempor incididunt "
+        " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " 
+        "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+        " consequat. Duis aute irure dolor in reprehenderit in voluptate "
+        "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint "
+        "occaecat cupidatat non proident, sunt in culpa qui "
+        " officia deserunt mollit anim id est laborum."
+        "Lorem ipsum dolor sit amet," 
+        " consectetur adipiscing elit, sed do eiusmod tempor incididunt "
+        " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " 
+        "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+        " consequat. Duis aute irure dolor in reprehenderit in voluptate "
+        "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint "
+        "occaecat cupidatat non proident, sunt in culpa qui "
+        " officia deserunt mollit anim id est laborum."
+        "Lorem ipsum dolor sit amet," 
+        " consectetur adipiscing elit, sed do eiusmod tempor incididunt "
+        " ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis " 
+        "nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo"
+        " consequat. Duis aute irure dolor in reprehenderit in voluptate "
+        "velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint "
+        "occaecat cupidatat non proident, sunt in culpa qui "
+        " officia deserunt mollit anim id est laborum.", width=500,font=appHighlightFont)
+        label_2.pack()
+        lable = tk.Label(self , text="",font=appHighlightFont)
+        lable.pack()
+        lable = tk.Label(self , text="",font=appHighlightFont)
+        lable.pack()
+        tk.Button(self, text="NEXT>>",
+                  command=lambda: master.switch_frame(PageOne)).pack()
+
+       
+                 
+
+class PageOne(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        appHighlightFont = font.Font(family='Helvetica', size=14)
+        tk.Label(self, text="Enter Username",  font=appHighlightFont).grid(row=10,column=0)
+        tk.Label(self, text="Enter Password", font=appHighlightFont).grid(row=12,column=0)
+        tk.Label(self, text="Enter Person's username",  font=appHighlightFont).grid(row=14,column=0)
+        entry_1 = tk.Entry(self, width="30")
+        entry_2 = tk.Entry(self, show='*', width="30")
+        entry_3 = tk.Entry(self, width="30")
+        for i in (0,1,2,3,4,5,6,7,8,9,11,13,15,16):
+            tk.Label(self,text="", width=15).grid(row=i,column=0)
+        entry_1.grid(row=10,column=7)
+        entry_2.grid(row=12,column=7)
+        entry_3.grid(row=14,column=7)
+        tk.Button(self,font=appHighlightFont,
+                          text='Continue',
+                          command=lambda:[self.value(entry_1.get(),entry_2.get(),entry_3.get()),
+                                          master.switch_frame(PageTwo)]).grid(row = 17,column = 3)
+    def value(self,val1,val2,val3):
+        data.append(val1)
+        data.append(val2)
+        data.append(val3)
+
+class PageTwo(tk.Frame):
+    def __init__(self, master):
+        tk.Frame.__init__(self, master)
+        appHighlightFont = font.Font(family='Helvetica', size=17)
+        for i in (0,1,2,3,4,5,6,8,10,12,14,16,17,19,20):
+            tk.Label(self,text="", width=15).grid(row=i,column=0)
+        tk.Label(self, text="What DO you want to Do???",font=appHighlightFont).grid(row=7 ,sticky=tk.W)
+        appHighlightFont = font.Font(family='Helvetica', size=14)
+        var1 = tk.IntVar()
+        tk.Checkbutton(self, text="Follow" ,font=appHighlightFont, variable=var1).grid(row=9 ,sticky=tk.W)
+        var2 = tk.IntVar()
+        tk.Checkbutton(self, text="Unfollow",font=appHighlightFont, variable=var2).grid(row=11,sticky=tk.W)
+        var3 = tk.IntVar()
+        tk.Checkbutton(self, text="Like Pictures",font=appHighlightFont, variable=var3).grid(row=13,sticky=tk.W)
+        var4 = tk.IntVar()
+        tk.Checkbutton(self, text="Download All pictures",font=appHighlightFont, variable=var4).grid(row=15,sticky=tk.W)
+        tk.Button(self,font=appHighlightFont,
+                          text='Continue',
+                          command=lambda:[self.value("{}{}{}{}".format(var1.get(),var2.get(),var3.get(),var4.get())),
+                                          app.destroy()]).grid(row = 18)
+    def value(self,order):
+        data.append(order)
+
+
+    
+
+
+if __name__ == "__main__":
+    data = list()
+    app = App()
+    app.title('InstaBot_Python')
+    app.geometry('600x600')
+    app.iconbitmap('anonymous_face_mask_icon_123833.ico')
+    app.mainloop()
+    print(data)
+    InstagramBot(data)
+
+
